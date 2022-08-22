4d35c189d7152181b2b3fcafee7cfb470ee9e09a
JETSlN <68126549+JETSlN@users.noreply.github.com>
2022-08-22 14:11:40 -0400

    images

--- bakery/bakery/settings.py
+++ bakery/bakery/settings.py
@@ -120,14 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "bakery/homepage/static"),)
-STATIC_ROOT = os.path.join(BASE_DIR, "/bakery/homepage/static")
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "homepage/static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, "/homepage/static")
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "/bakery/homepage/static/homepage/media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "/homepage/static/homepage/media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

bc48e5fb4d68e620ffb5dbc4c610b42cc4e30b97
JETSlN <68126549+JETSlN@users.noreply.github.com>
2022-08-24 00:29:27 -0400

    updated whitenoise version

--- bakery/homepage/views.py
+++ bakery/homepage/views.py
@@ -1,10 +1,90 @@
+import imp
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.conf import settings
+from django.views import View
+
+"""
+class index(View):
+    def get(self, request):
+        return render(request, 'homepage/index.html')
+
+class story(View):
+    def get(self, request):
+        return render(request, 'homepage/story.html')
+
+class contact(View):
+    def get(self, request):
+        return render(request, 'homepage/index.html')
+    def post(self, request):
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message_tel = request.POST['message-tel']
+        message = request.POST['message']
+        context = {
+            'message_name': message_name,
+            'message_email': message_email,
+            'message_tel': message_tel,
+            'message': message,
+        }
+
+        send_mail(
+            message_name, # subject
+            message + "\n Phone Number: " + message_tel + '\n' + "Email: " + 
+            message_email, # message
+            message_email, # from email
+            ['teaflowerbakery@gmail.com'], # to email
+
+        )
+        return render(request, 'homepage/contact.html', context)
+
+class bread(View):
+    def get(self, request):
+        return render(request, 'homepage/bread.html')
+
+class drink(View):
+    def get(self, request):
+        return render(request, 'homepage/drink.html')
 
 
+class cookie(View):
+    def get(self, request):
+        return render(request, 'homepage/cookie.html')
 
+
+class pastry(View):
+    def get(self, request):
+        return render(request, 'homepage/pastry.html')
+
+
+class cake(View):
+    def get(self, request):
+        return render(request, 'homepage/cake.html')
+    
+    def post(self, request):
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message_tel = request.POST['message-tel']
+        message = request.POST['message']
+        context = {
+            'message_name': message_name,
+            'message_email': message_email,
+            'message_tel': message_tel,
+            'message': message,
+        }
+
+        send_mail(
+            message_name, # subject
+            message + "\n Phone Number: " + message_tel + '\n' + "Email: " + 
+            message_email, # message
+            message_email, # from email
+            ['teaflowerbakery@gmail.com'], # to email
+
+        )
+        return render(request, 'homepage/cake.html')
+"""
+#"""
 def index(request):
     return render(request, 'homepage/index.html')
 
@@ -45,9 +125,6 @@ def drink(request):
 def cookie(request):
     return render(request, 'homepage/cookie.html')
 
-def loave(request):
-    return render(request, 'homepage/loave.html')
-
 def pastry(request):
     return render(request, 'homepage/pastry.html')
 
@@ -75,4 +152,4 @@ def cake(request):
         return render(request, 'homepage/cake.html')
     else:
         return render(request, 'homepage/cake.html')
-    
+#"""
